9b75b0f0e3ad02cc283a82191f041124bd1bff39
nimagalunaziya <nimagalunaziya@gmail.com>
2024-05-27 09:24:13 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCalendarAlt } from '@fortawesome/free-solid-svg-icons';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='timer'>
+        <div><h1>Countdown <span style={{ color: "purple" }}>Timer</span></h1></div>
+        <div className='dmy'>
+          < FontAwesomeIcon icon={faCalendarAlt} className='calendar-icon' />
+          <input type='date' className='date-input' />
+        </div>
+        <button className='button'>Start Timer</button>
+        <div className='box'>
+          <div className='days'> 
+            <h1>0 </h1>  
+            <h3 style={{ color: "whitesmoke" }}>days</h3>
+          </div>
+          <div className='hours'>
+            <h1>0</h1>
+            <h3 style={{ color: "whitesmoke" }}>Hours</h3>
+          </div>
+          <div className='min'> 
+            <h1>0</h1>
+            <h3 style={{ color: "whitesmoke" }}>Minutes</h3>
+          </div>
+          <div className='sec'> 
+            <h1>0</h1>
+            <h3 style={{ color: "whitesmoke" }}>Seconds</h3>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
