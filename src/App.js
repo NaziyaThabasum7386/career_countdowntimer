e42900ac4ed24aef18928cc47b3e377c9765a93e
nimagalunaziya <nimagalunaziya@gmail.com>
2024-05-27 12:23:30 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,17 +1,34 @@
-import React from 'react';
 import './App.css';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCalendarAlt } from '@fortawesome/free-solid-svg-icons';
+import React, { useState, useEffect } from 'react';
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
 
 function App() {
+ const [days,setDays] = useState(0);
+ const [hours,setHours] = useState(0);
+ const [min, setMIn] = useState(0);
+ const [sec, useSec] = useState(0);
+ const [totalsec, setTotalsec] = useState(0);
+const [selectedDate, setSelectedDate] = useState(null);
+
+
+
+
   return (
     <div className="App">
       <div className='timer'>
         <div><h1>Countdown <span style={{ color: "purple" }}>Timer</span></h1></div>
         <div className='dmy'>
-          < FontAwesomeIcon icon={faCalendarAlt} className='calendar-icon' />
-          <input type='date' className='date-input' />
-        </div>
+          <DatePicker
+           selected={selectedDate}
+           onChange={ date => setSelectedDate(date)} 
+           dateFormat={dd/mm/yyyy}
+           minDate={new Date()}
+           showYearDropdown
+           scrollableMonthYearDropdown
+           />
+          
+           </div>
         <button className='button'>Start Timer</button>
         <div className='box'>
           <div className='days'> 
